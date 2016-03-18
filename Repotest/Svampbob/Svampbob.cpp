11ef020e974548bdae8bac9ebaccc68593b407b1
Oskar Wåglund <oskar.waglund@st.se>
2016-03-18 14:27:24 +0100

    Added the Crusty Crab

--- Repotest/Svampbob/Svampbob.cpp
+++ Repotest/Svampbob/Svampbob.cpp
@@ -5,6 +5,33 @@
 #include <iostream>
 #include <string>
 
+void crustyCrab(){
+	std::cout << "Welcome to the Crusty Crab!" << std::endl;
+	std::cout << "***Menu***" << std::endl;
+	std::cout << "1. Crab burgers" << std::endl;
+	std::cout << "2. Crab fries" << std::endl;
+	std::cout << "3. Crab cola" << std::endl << std::endl;
+
+	int order;
+	std::cout << "Please make your order!" << std::endl;
+	std::cin >> order;
+
+	std::string nameOfOrder;
+	if (order == 1)
+		nameOfOrder = "Crab burgers";
+	else if (order == 2)
+		nameOfOrder = "Crab fries";
+	else if (order == 3)
+		nameOfOrder = "Crab cola";
+	else
+		nameOfOrder = "";
+		
+	if (nameOfOrder.length() > 0)
+		std::cout << "One " << nameOfOrder << " coming up!" << std::endl;
+	else
+		std::cout << "No crabs for you!" << std::endl;
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout << "Hello Bikinibotten!" << std::endl;
@@ -27,9 +54,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 	
-
+	crustyCrab();
 	
-
 	return 0;
 }
 
